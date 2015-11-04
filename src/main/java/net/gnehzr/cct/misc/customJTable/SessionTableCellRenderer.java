4c61e8709b0ab65999fa6e68f89628082a910520
OneHalf_3544 <onehalf354@gmail.com>
2015-11-04 16:49:07 +0300

    fix session list highlighting

--- src/main/java/net/gnehzr/cct/misc/customJTable/SessionTableCellRenderer.java
+++ src/main/java/net/gnehzr/cct/misc/customJTable/SessionTableCellRenderer.java
@@ -1,11 +1,11 @@
 package net.gnehzr.cct.misc.customJTable;
 
+import net.gnehzr.cct.statistics.Session;
 import net.gnehzr.cct.statistics.SessionsList;
 
 import javax.swing.*;
 import javax.swing.table.TableCellRenderer;
 import java.awt.*;
-import java.util.Date;
 
 public class SessionTableCellRenderer implements TableCellRenderer {
 
@@ -18,24 +18,23 @@ public class SessionTableCellRenderer implements TableCellRenderer {
 	@Override
 	public Component getTableCellRendererComponent(JTable table, Object value, boolean isSelected,
 												   boolean hasFocus, int row, int column) {
-		JLabel label = new JLabel();
-		if (value == null) {
-			label.setText(new Date().toString());
-		}
-		else {
-			label.setText(value.toString());
-			label.setHorizontalAlignment(SwingConstants.RIGHT);
-			label.setBorder(BorderFactory.createEmptyBorder(0, 5, 0, 5));
-		}
+		Session session = sessionsList.getNthSession(table.convertRowIndexToModel(row));
 
-		//emphasize the current session
-		if (sessionsList.getCurrentSession() == sessionsList.getNthSession(row)) {
-			label.setBackground(Color.GREEN);
-		}
-		else {
-			label.setBackground(Color.WHITE);
-		}
+		JLabel label = new JLabel();
+		label.setOpaque(true);
+
+		boolean isCurrentSession = sessionsList.getCurrentSession() == session;
+		label.setBackground(getBackgroundColor(table, isCurrentSession, isSelected));
+
+		label.setText(value.toString());
+		label.setHorizontalAlignment(SwingConstants.RIGHT);
+		label.setBorder(BorderFactory.createEmptyBorder(0, 5, 0, 5));
 
 		return label;
+	}
+
+	private Color getBackgroundColor(JTable table, boolean isCurrentSession, boolean isSelected) {
+		Color result = isCurrentSession ? Color.GREEN : table.getBackground();
+		return isSelected ? result.darker() : result;
 	}
 }
